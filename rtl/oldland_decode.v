0ec1f0154129c916aa4e178ac0c5ac7782f1f087
Jamie Iles <jamie.iles@oracle.com>
2013-05-22 15:32:00 +0100

    rtl: add a tool for creating verilog defines and use it.

--- rtl/oldland_decode.v
+++ rtl/oldland_decode.v
@@ -19,7 +19,10 @@ module oldland_decode(input wire [31:0] instr,
 		      output wire mem_store,
 		      output wire branch_ra);
 
-assign alu_opc = instr[31:30] == 2'b00 ? instr[29:26] : 4'b0;
+wire [1:0] class = instr[31:30];
+wire [3:0] opcode = instr[29:26];
+
+assign alu_opc = class == `CLASS_ARITH ? opcode : 4'b0;
 
 assign ra_sel = instr[5:3];
 assign rb_sel = instr[2:0];
@@ -31,7 +34,7 @@ assign rd_sel = instr[8:6];
  * where we intentionally discard the result and load operations where the
  * register is update by the LSU later.
  */
-assign update_rd = instr[31:30] == 2'b0 && instr[29:26] != 4'b1011;
+assign update_rd = class == `CLASS_ARITH && opcode != `OPCODE_CMP;
 
 assign branch_condition = instr[28:26];
 
@@ -43,15 +46,15 @@ wire [31:0] imm24 = {{6{instr[23]}}, instr[23:0], 2'b00};
  * The output immediate - either one of the sign extended immediates or
  * a special case for movhi - the 16 bit immediate shifted left 16 bits.
  */
-assign imm32 = (instr[31:30] == 2'b01) ? imm24 :
-	       (instr[31:26] == 6'b001011) ? {instr[25:10], 16'b0} : imm16;
+assign imm32 = (class == `CLASS_BRANCH) ? imm24 :
+	       (instr[31:26] == `OPCODE_MOVHI) ? {instr[25:10], 16'b0} : imm16;
 
-assign branch_ra = instr[31:30] == 2'b01 && instr[25];
+assign branch_ra = class == `CLASS_BRANCH && instr[25];
 	
-assign alu_op1_ra = (instr[31:30] == 2'b00 || instr[31:30] == 2'b10);
-assign alu_op2_rb = (instr[31:30] == 2'b00 && instr[9]);
+assign alu_op1_ra = (class == `CLASS_ARITH || class == `CLASS_MEM);
+assign alu_op2_rb = (class == `CLASS_ARITH && instr[9]);
 
-assign mem_load = instr[31:30] == 2'b10 && instr[28] == 1'b0;
-assign mem_store = instr[31:30] == 2'b10 && instr[28] == 1'b1;
+assign mem_load = class == `CLASS_MEM && instr[28] == 1'b0;
+assign mem_store = class == `CLASS_MEM && instr[28] == 1'b1;
 
 endmodule
