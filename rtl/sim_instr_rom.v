94dd53ab8fbf1497b47748134d2ca2fd20270bc0
Jamie Iles <jamie@jamieiles.com>
2013-05-27 19:58:39 +0100

    rtlsim: allow rom file to be specified at runtime.

--- rtl/sim_instr_rom.v
+++ rtl/sim_instr_rom.v
@@ -3,9 +3,15 @@ module sim_instr_rom(input wire clk,
 		     output reg [31:0] data);
 
 reg [7:0] instr_rom [127:0];
+reg [8 * 128:0] rom_filename;
 
 initial begin
-	$readmemh("rom.hex", instr_rom);
+	if (!$value$plusargs("romfile=%s", rom_filename) ||
+	    rom_filename == 0) begin
+		$display("+romfile=PATH_TO_ROM.hex required");
+		$finish;
+	end
+	$readmemh(rom_filename, instr_rom);
 	data = 32'hffffffff;
 end
 
