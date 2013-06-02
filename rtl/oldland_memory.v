4d850efd0473f4a36f30ce474b24e34cfab10dc3
Jamie Iles <jamie@jamieiles.com>
2013-06-03 00:15:59 +0100

    rtl/memory: implement byte enables and bytelane shifts.
    This means we can do real 8-bit and 16-bit accesses without manual
    masking and shifting.

--- rtl/oldland_memory.v
+++ rtl/oldland_memory.v
@@ -12,9 +12,9 @@ module oldland_memory(input wire clk,
 		      output reg complete,
 		      /* Signals to data bus */
 		      output wire [31:0] d_addr,
-		      output wire [3:0] d_bytesel,
+		      output reg [3:0] d_bytesel,
 		      output wire d_wr_en,
-		      output wire [31:0] d_wr_val,
+		      output reg [31:0] d_wr_val,
 		      input wire [31:0] d_data,
 		      output wire d_access);
 
@@ -22,23 +22,54 @@ initial begin
 	update_rd_out = 1'b0;
 	rd_sel_out = 3'b0;
 	wr_val_bypass = 32'b0;
+	d_bytesel = 4'b0;
+	d_wr_val = 32'b0;
+	mem_rd_val = 32'b0;
 end
 
 reg [31:0] wr_val_bypass;
+reg [31:0] mem_rd_val;
 
-assign d_addr = addr;
-assign d_bytesel = 4'b1111;
+assign d_addr = {addr[31:2], 2'b00};
 assign d_wr_en = store;
-assign d_wr_val = wr_val;
 assign d_access = load | store;
 
-assign reg_wr_val = complete ? d_data : wr_val_bypass;
+assign reg_wr_val = complete ? mem_rd_val : wr_val_bypass;
 
 always @(posedge clk) begin
 	complete <= load | store;
 	wr_val_bypass <= wr_val;
-	update_rd_out <= update_rd;
 	rd_sel_out <= rd_sel;
+	if (load | store)
+		update_rd_out <= 1'b1;
+	else
+		update_rd_out <= update_rd;
+end
+
+/* Byte enables and rotated data write value. */
+always @(*) begin
+	case (width)
+	2'b10: begin
+		d_bytesel = 4'b1111;
+		d_wr_val = wr_val;
+		mem_rd_val = d_data;
+	end
+	2'b01: begin
+		d_bytesel = 4'b0011 << (addr[1] * 2);
+		d_wr_val = wr_val << (addr[1] * 16);
+		mem_rd_val = d_data >> (addr[1] * 16);
+	end
+	2'b00: begin
+		d_bytesel = 4'b0001 << addr[1:0];
+		d_wr_val = wr_val << (addr[1:0] * 8);
+		mem_rd_val = d_data >> (addr[1:0] * 8);
+	end
+	default: begin
+		d_bytesel = 4'b1111;
+		d_wr_val = wr_val;
+		mem_rd_val = d_data;
+	end
+	endcase
 end
 
 endmodule
