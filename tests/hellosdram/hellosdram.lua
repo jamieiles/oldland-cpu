b7d8e7a7613ef184f9c3f1ca68d072d527ff00c5
Jamie Iles <jamie@jamieiles.com>
2013-06-05 23:28:20 +0100

    tests: add a UART test that copies the string from SDRAM.

---
+++ tests/hellosdram/hellosdram.lua
@@ -0,0 +1,22 @@
+require "bit32"
+
+CYCLE_LIMIT = 1000
+BINARY = "hellosdram.bin"
+
+uart_data = ""
+
+function data_write_hook(addr, width, value)
+	if addr == 0x80000000 and width == 32 then
+		uart_data = uart_data .. string.format("%c", bit32.band(value, 0xff))
+	end
+end
+
+function run()
+	run_cpu()
+end
+
+function validate_result()
+	if uart_data ~= "SDRAM!" then
+		sim.err("output string does not match - \"" .. uart_data .. "\"")
+	end
+end
