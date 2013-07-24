f0779aafa06394b62243b1d16bb01813607c0091
Jamie Iles <jamie@jamieiles.com>
2013-07-24 23:57:03 +0100

    debugger: add a Lua interface.
    This allows some scripting through the target table.  We'll use a Lua
    script to make something that is a bit more user friendly though.

--- debugger/debugger.c
+++ debugger/debugger.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 
+#include <assert.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -12,11 +13,20 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include <lua5.2/lua.h>
+#include <lua5.2/lauxlib.h>
+#include <lua5.2/lualib.h>
+
 #include "protocol.h"
 
 struct target {
 	int fd;
 };
+
+static struct target *target;
 
 int target_exchange(const struct target *t, const struct dbg_request *req,
 		    struct dbg_response *resp)
@@ -152,16 +162,91 @@ int open_server(const char *hostname, const char *port)
 	return fd;
 }
 
-int main(void)
+struct target *target_alloc(void)
 {
-	struct target t;
+	struct target *t = calloc(1, sizeof(*t));
 
-	t.fd = open_server("localhost", "36000");
-	if (t.fd < 0)
+	if (!t)
+		err(1, "failed to allocate target");
+
+	t->fd = open_server("localhost", "36000");
+	if (t->fd < 0)
 		err(1, "failed to connect to server");
 
-	if (dbg_step(&t))
-		err(1, "failed to step target");
+	return t;
+}
+
+static int lua_step(lua_State *L)
+{
+	if (dbg_step(target))
+		warnx("failed to step target");
+
+	return 0;
+}
+
+static int lua_stop(lua_State *L)
+{
+	if (dbg_stop(target))
+		warnx("failed to step target");
+
+	return 0;
+}
+
+static int lua_run(lua_State *L)
+{
+	if (dbg_run(target))
+		warnx("failed to step target");
+
+	return 0;
+}
+
+static int lua_read_reg(lua_State *L)
+{
+	uint32_t v;
+	lua_Integer regnum;
+
+	regnum = lua_tointeger(L, 1);
+	if (dbg_read_reg(target, regnum, &v))
+		warnx("failed to read register %u", (unsigned)regnum);
+	lua_pop(L, 1);
+	lua_pushinteger(L, v);
+
+	return 1;
+}
+
+static const struct luaL_Reg dbg_funcs[] = {
+	{ "step", lua_step },
+	{ "run", lua_run },
+	{ "stop", lua_stop },
+	{ "read_reg", lua_read_reg },
+	{}
+};
+
+int main(void)
+{
+	lua_State *L = luaL_newstate();
+
+	assert(L);
+
+	luaL_openlibs(L);
+	luaL_newlib(L, dbg_funcs);
+	lua_setglobal(L, "target");
+
+	target = target_alloc();
+
+	stifle_history(1024);
+
+	for (;;) {
+		char *line = readline("oldland> ");
+
+		if (!line)
+			break;
+
+		if (luaL_dostring(L, line))
+			warnx("error: %s", lua_tostring(L, -1));
+
+		add_history(line);
+	}
 
 	return 0;
 }
