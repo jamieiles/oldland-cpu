a1f4224c650d99dfd504eef667af84c619e65e9d
Jamie Iles <jamie@jamieiles.com>
2013-08-28 21:26:42 +0100

    debugger: cleanup lua script.

--- debugger/oldland-debug-ui.lua
+++ debugger/oldland-debug-ui.lua
@@ -1,52 +1,30 @@
-function step()
-	target.step()
-end
-
-function stop()
-	target.stop()
-end
-
-function run()
-	target.run()
-end
+step = target.step
+stop = target.stop
+run = target.run
+write_reg = target.write_reg
+write32 = target.write32
+write16 = target.write16
+write8 = target.write8
+loadelf = target.loadelf
 
 function read_reg(reg)
 	print(string.format("%08x", target.read_reg(reg)))
-end
-
-function write_reg(reg, val)
-	target.write_reg(reg, val)
 end
 
 function read32(addr)
 	print(string.format("%08x", target.read32(addr)))
 end
 
-function write32(addr, val)
-	target.write32(addr, val)
-end
-
 function read16(addr)
 	print(string.format("%04x", target.read16(addr)))
-end
-
-function write16(addr, val)
-	target.write16(addr, val)
 end
 
 function read8(addr)
 	print(string.format("%02x", target.read8(addr)))
 end
 
-function write8(addr, val)
-	target.write8(addr, val)
-end
-
-function loadelf(path)
-	target.loadelf(path)
-end
-
-regnames = { 'r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'sp', 'lr', 'pc' }
+regnames = { 'r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7',
+	     'r8', 'r9', 'r10', 'r11', 'r12', 'fp', 'sp', 'lr', 'pc' }
 
 function regs()
 	for idx, name in ipairs(regnames) do
