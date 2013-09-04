266ce0e6f8538561585dc2ce008cb6e2bffc7235
Jamie Iles <jamie@jamieiles.com>
2013-09-04 13:09:39 +0100

    tests: add push/pop macros for convenience.

--- tests/target/common.s
+++ tests/target/common.s
@@ -22,3 +22,13 @@
 .macro	FAILURE
 	TESTPOINT	TP_FAILURE, 0
 .endm
+
+.macro	push	reg
+	str32	\reg, [$sp, 0]
+	sub	$sp, $sp, 4
+.endm
+
+.macro	pop	reg
+	add	$sp, $sp, 4
+	ldr32	\reg, [$sp, 0]
+.endm
