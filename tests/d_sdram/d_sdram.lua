34a9d15722964b1639d349bf5ee37b6ed33dbb39
Jamie Iles <jamie@jamieiles.com>
2013-09-13 00:47:34 +0100

    tests: add a simple sdram test.

---
+++ tests/d_sdram/d_sdram.lua
@@ -0,0 +1,28 @@
+require "common"
+
+MAX_CYCLE_COUNT = 10000
+
+connect("localhost", "36000")
+loadelf("d_sdram")
+
+function validate_sdram()
+	addr = 0x20000000
+	data = ""
+	repeat
+		v = target.read8(addr)
+		addr = addr + 1
+		if v > 0 then data = data .. string.format("%c", v) end
+	until v == 0
+
+	if data ~= "Hello, world!" then
+		print(string.format("Unexpected output \"%s\"", data))
+		return -1
+	end
+end
+
+expect_testpoints = {
+	{ TP_USER, 0, validate_sdram },
+	{ TP_SUCCESS, 0 },
+}
+
+return run_testpoints(expect_testpoints)
---
+++ tests/d_sdram/d_sdram.s
@@ -0,0 +1,38 @@
+.include "common.s"
+
+.globl _start
+_start:
+	/* Wait for SDRAM to initialize. */
+	movhi	$r0, 0x8000
+	orlo	$r0, $r0, 0x1000
+1:
+	ldr32	$r1, [$r0, 0]
+	cmp	$r1, 0x0
+	beq	1b
+
+	movhi	$r0, %hi(hello_world)
+	orlo	$r0, $r0, %lo(hello_world)
+
+	movhi	$r1, %hi(end)
+	orlo	$r1, $r1, %lo(end)
+
+	movhi	$r3, 0x2000
+
+2:
+	cmp	$r0, $r1
+	beq	success
+
+	ldr8	$r2, [$r0, 0]
+	str8	$r2, [$r3, 0]
+	add	$r0, $r0, 1
+	add	$r3, $r3, 1
+
+	b	2b
+
+success:
+	TESTPOINT	TP_USER, 0
+	SUCCESS
+
+hello_world:
+	.asciz "Hello, world!"
+end:
