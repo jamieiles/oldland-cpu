80dd470da7282ea4f4eb48271bcefe56af899297
Jamie Iles <jamie@jamieiles.com>
2013-10-03 17:46:11 +0100

    tests/badaddr: set the exception table and stack.

--- tests/badaddr/badaddr.s
+++ tests/badaddr/badaddr.s
@@ -2,6 +2,13 @@
 
 .globl _start
 _start:
+	movhi	$r0, %hi(ex_table)
+	orlo	$r0, $r0, %lo(ex_table)
+	scr	0, $r0
+
+	movhi		$sp, %hi(stack_top)
+	orlo		$sp, $sp, %lo(stack_top)
+
 	movhi	$r0, 0x9000 /* Unmapped address. */
 	ldr32	$r1, [$r0, 0]
 
@@ -34,3 +41,8 @@ ex_table:
 	b	bad_vector	/* IRQ */
 	b	bad_vector	/* IFETCH_ABORT */
 	b	data_abort	/* DATA_ABORT */
+
+.rept	32
+	.long		0
+.endr
+stack_top:
