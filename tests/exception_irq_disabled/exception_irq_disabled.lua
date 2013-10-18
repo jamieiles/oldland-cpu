3bfd24c15283d80c1ba39d8adbce5496c61f0e5c
Jamie Iles <jamie@jamieiles.com>
2013-10-18 20:23:45 +0100

    tests: make sure that interrupts are disabled inside exceptions handlers.

---
+++ tests/exception_irq_disabled/exception_irq_disabled.lua
@@ -0,0 +1,12 @@
+require "common"
+
+MAX_CYCLE_COUNT = 128
+
+connect_and_load("exception_irq_disabled")
+
+expect_testpoints = {
+	{ TP_USER, 0 },
+	{ TP_SUCCESS, 0 },
+}
+
+return run_testpoints(expect_testpoints)
---
+++ tests/exception_irq_disabled/exception_irq_disabled.s
@@ -0,0 +1,46 @@
+.include "common.s"
+
+.globl _start
+_start:
+	movhi	$r0, %hi(ex_table)
+	orlo	$r0, $r0, %lo(ex_table)
+	scr	0, $r0
+
+	movhi	$sp, %hi(stack_top)
+	orlo	$sp, $sp, %lo(stack_top)
+
+	/* Enable interrupts. */
+	gcr	$r0, 1
+	bst	$r0, $r0, 4
+	scr	1, $r0
+
+	swi	0
+
+	FAILURE
+
+swi_vector:
+	TESTPOINT	TP_USER, 0
+	gcr	$r0, 1
+	and	$r0, $r0, 16
+	cmp	$r0, 16
+	beq	failed
+	SUCCESS
+failed:
+	FAILURE
+
+bad_vector:
+	b	bad_vector
+
+	.balign	64
+ex_table:
+	b	bad_vector	/* RESET */
+	b	bad_vector	/* ILLEGAL_INSTR */
+	b	swi_vector	/* SWI */
+	b	bad_vector	/* IRQ */
+	b	bad_vector	/* IFETCH_ABORT */
+	b	bad_vector	/* DATA_ABORT */
+
+.rept	32
+	.long		0
+.endr
+stack_top:
