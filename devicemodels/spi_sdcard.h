31d326c1a59bdcef93864cc4bc2bfecf2e393ab3
Jamie Iles <jamie@jamieiles.com>
2014-09-19 13:19:34 +0100

    devicemodels: make into a library.
    This will be used for linking the verilator model.

--- devicemodels/spi_sdcard.h
+++ devicemodels/spi_sdcard.h
@@ -3,11 +3,19 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct spi_sdcard;
 
 struct spi_sdcard *spi_sdcard_new(const char *path);
 
 uint8_t spi_sdcard_next_byte_to_master(struct spi_sdcard *sd);
 void spi_sdcard_next_byte_to_slave(struct spi_sdcard *sd, uint8_t v);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif /* __SPI_SDCARD_H__ */
--- devicemodels/uart.h
+++ devicemodels/uart.h
@@ -1,11 +1,19 @@
 #ifndef __UART_H__
 #define __UART_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct uart_data {
 	int fd;
 };
 
 int create_pts(void);
 int sim_is_interactive(void);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif /* __UART_H__ */
