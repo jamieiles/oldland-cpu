06427cb2413a401cab27bb1586d24452984454c9
Jamie Iles <jamie@jamieiles.com>
2014-10-12 12:45:39 +0100

    tests: test all conditional branches.

--- tests/branches/branches.s
+++ tests/branches/branches.s
@@ -40,37 +40,83 @@
 2:
 .endm
 
-.globl _start
-_start:
-	compare_uints	1, 2
-	compare_uints	1, 0xfffffffe
-	compare_uints	0, 0xffffffff
-
-	compare_ints	1, 2
-	compare_ints	-1, 1
-	compare_ints	-1, 0
-	compare_ints	-2, -1
-	compare_ints	0x80000000, 0x7fffffff
-
-	/* Unsigned BGTE */
-	mov	$r0, 1
-	mov	$r1, 1
-	cmp	$r1, $r0
-	bgte	1f
+.macro	taken	branch, a, b
+	movhi	$r0, %hi(\a)
+	orlo	$r0, $r0, %lo(\a)
+	movhi	$r1, %hi(\b)
+	orlo	$r1, $r1, %lo(\b)
+	cmp	$r0, $r1
+	\branch	1f
 	FAILURE
 1:
+.endm
 
-	mov	$r0, 1
-	mov	$r1, 2
-	cmp	$r1, $r0
-	bgte	2f
+.macro	not_taken	branch, a, b
+	movhi	$r0, %hi(\a)
+	orlo	$r0, $r0, %lo(\a)
+	movhi	$r1, %hi(\b)
+	orlo	$r1, $r1, %lo(\b)
+	cmp	$r0, $r1
+	\branch	1f
+	b	2f
+1:
 	FAILURE
 2:
+.endm
 
-	mov	$r0, 2
-	mov	$r1, 1
-	cmp	$r1, $r0
-	bgte	failure
+.globl _start
+_start:
+	taken		beq, 0, 0
+	not_taken	beq, 0, 1
+
+	taken		bne, 0, 1
+	not_taken	bne, 0, 0
+
+	/* Unsigned branch-greater-than. */
+	taken		bgt, 1, 0
+	taken		bgt, -1, 0
+	not_taken	bgt, 0, 1
+	not_taken	bgt, 1, 1
+
+	/* Unsigned branch-less-than. */
+	taken		blt, 0, 1
+	not_taken	blt, 1, 0
+	not_taken	blt, 1, 1
+	not_taken	blt, -1, 0
+
+	/* Signed branch-greater-than. */
+	taken		bgts, -1, -2
+	taken		bgts, 1, -1
+	not_taken	bgts, 0, 0
+	not_taken	bgts, -2, -1
+
+	/* Signed branch-less-than. */
+	taken		blts, -2, -1
+	taken		blts, -2, 2
+	not_taken	blts, 2, -2,
+	not_taken	blts, 0, 0
+
+	/* Unsigned branch-greater-equal. */
+	taken		bgte, 1, 0
+	taken		bgte, 1, 1
+	not_taken	bgte, 0, 1
+
+	/* Signd branch-greater-equal. */
+	taken		bgtes, 1, 0
+	taken		bgtes, 0, -1
+	taken		bgtes, 0, 0
+	not_taken	bgtes, 1, 2
+
+	/* Unsigned branch-less-equal. */
+	taken		blte, 0, 1
+	taken		blte, 1, 1
+	not_taken	blte, 1, 0
+
+	/* Signd branch-less-equal. */
+	taken		bltes, 0, 1
+	taken		bltes, -1, 0
+	taken		bltes, 0, 0
+	not_taken	bltes, 2, 1
 
 	SUCCESS
 
